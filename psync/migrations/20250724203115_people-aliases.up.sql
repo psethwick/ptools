df6a59dce4fb6f30c388b9ee610cd6ee1a850fa4
Seth Rider <seth.rider@gmail.com>
2025-07-26 21:34:29 +0100

    foreign key requires all of composite primary
    lets yolo for now

--- psync/migrations/20250724203115_people-aliases.up.sql
+++ psync/migrations/20250724203115_people-aliases.up.sql
@@ -1,7 +1,5 @@
 CREATE TABLE alias (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   person_id TEXT NOT NULL,
-  aka_id TEXT NOT NULL,
-  FOREIGN KEY (person_id) REFERENCES person (id),
-  FOREIGN KEY (aka_id) REFERENCES person (id)
+  aka_id TEXT NOT NULL
 );
